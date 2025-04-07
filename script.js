eb75ad94d7981ba0bfd242a5fc5571c8c08b2703
Nishtha Chauhan <160705830+nishthachauhan2708@users.noreply.github.com>
2025-04-07 19:38:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".text",{
+    strings:["Frontend Developer", "Wev Developer", "Computer Science"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+
+});
